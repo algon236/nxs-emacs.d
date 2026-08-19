9802cf31e16f037e22b8ec4e102604613a4a6ae2
Niels Søndergaard <306411913+algon236@users.noreply.github.com>
2026-08-19 02:51:20 +0200

    Publish current Emacs fixes and people groups

---
+++ var/templates/template.el
@@ -0,0 +1,33 @@
+;;; {{filename}} --- {{description}}  -*- lexical-binding: t; byte-compile-warnings: (not free-vars unresolved make-local); -*-
+
+;; Copyright (C) Copenhagen,  {{year}}  Niels Søndergaard
+
+;; Author: Niels Søndergaard <niels@algon.dk>
+;; Keywords: {{keywords}}
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; {{point}}
+
+;;; Code:
+
+
+
+(provide '{{feature}})
+
+;;; {{filename}} ends here
